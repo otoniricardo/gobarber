f46945fa2147153b0638669fb837cf8f041bae27
otoniricardo <otoniricardo@gmail.com>
2020-04-01 19:19:51 -0300

    configura templates de email

--- back-end/src/app/controllers/Appointment.js
+++ back-end/src/app/controllers/Appointment.js
@@ -12,7 +12,10 @@ class AppointmentController {
   async delete(req, res) {
     const { id } = req.params;
     const appointment = await Appointment.findByPk(id, {
-      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+      include: [
+        { model: User, as: 'provider', attributes: ['name', 'email'] },
+        { model: User, as: 'user', attributes: ['name'] },
+      ],
       attributes: ['id', 'date', 'userId', 'providerId'],
     });
 
@@ -35,7 +38,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento Cancelado',
-      text: 'Voce tem um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(canceledAppointment);
