792068b85ee6629746551bc28d043a9769e9ae5f
otoniricardo <otoniricardo@gmail.com>
2020-04-01 13:10:50 -0300

    implementa schema para notificacoes no mongo db

--- back-end/src/app/controllers/Appointment.js
+++ back-end/src/app/controllers/Appointment.js
@@ -1,8 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async store(req, res) {
@@ -43,6 +46,18 @@ class AppointmentController {
       userId: req.userId,
       providerId,
       date: hourStart,
+    });
+
+    const { name } = await User.findByPk(req.userId, { attributes: ['name'] });
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${name} para ${formattedDate}`,
+      user: providerId,
     });
 
     return res.json(appointment);
