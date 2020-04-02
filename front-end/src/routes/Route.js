9d98fdc67d3a0a36bb4976cc4cd4d69415bb46ef
otoniricardo <otoniricardo@gmail.com>
2020-04-02 13:18:23 -0300

    Cria layouts

--- front-end/src/routes/Route.js
+++ front-end/src/routes/Route.js
@@ -1,20 +1,34 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Proptypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) return <Redirect to="/" />;
 
   if (signed && !isPrivate) return <Redirect to="/dashboard" />;
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
