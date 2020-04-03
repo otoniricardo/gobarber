5a0331eeebcc89e8699b4b60a5074ff9898045b8
otoniricardo <otoniricardo@gmail.com>
2020-04-02 23:23:09 -0300

    Implementa loading no login

--- front-end/src/store/modules/auth/sagas.js
+++ front-end/src/store/modules/auth/sagas.js
@@ -3,23 +3,27 @@ import { all, takeLatest, call, put } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { singInSuccess } from './actions';
+import { singInSuccess, singFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'session', { email, password });
+    const response = yield call(api.post, 'session', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('usuario não é prestador');
-    return;
+    if (!user.provider) {
+      console.tron.error('usuario não é prestador');
+      return;
+    }
+
+    yield put(singInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(singFailure());
   }
-
-  yield put(singInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
