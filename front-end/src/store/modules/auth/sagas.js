c9097911ca665805b45f7d6ffcf05ede21a8b0a6
otoniricardo <otoniricardo@gmail.com>
2020-04-02 21:19:29 -0300

    implementa autenticação

--- front-end/src/store/modules/auth/sagas.js
+++ front-end/src/store/modules/auth/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { singInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'session', { email, password });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('usuario não é prestador');
+    return;
+  }
+
+  yield put(singInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
